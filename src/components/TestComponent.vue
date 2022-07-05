9c611684c0a110455a026b2d0c1e3613204efdb8
Balazs <balazs@frog.net>
2022-07-05 09:02:46 +0100

    TestComponent
    created a test component

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="wrapper">
+        <h1>This is the component with the title of: {{ title }} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: "Storybook test",
+        }
+    }
+}
+</script>
+
+<style>
+    .wrapper {
+        border: 1px solid red; 
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TestComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import TestComponent from '@/components/TestComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    TestComponent
   }
 }
 </script>
